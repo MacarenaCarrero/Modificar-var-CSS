cab5fc2c56d371e14510440070fda89bff1cb6b9
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-07 20:51:10 +0200

    vaaar

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-// raton
+const rootStyles = document.documentElement.style;
+
 const mouseElement = document.getElementById('mouse');
+
 const redBoxElement = document.getElementById('redBox');
 const pixelsElement = document.getElementById('pixels');
+const buttonHexaElement = document.getElementById('buttonHexa');
+const buttonRgbElement = document.getElementById('buttonRgb');
 //
 
-mouseElement.addEventListener('mousemove', mouseFollower);
+// unir la barrita al scroll
+const modifyRedBar = event => {
+  const totalScroll = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const cuantityScroll = window.scrollY;
+};
+
+const changeColorHexa = () => {};
+const changeColorRgb = () => {};
+
+// mouseElement.addEventListener('mousemove', );
