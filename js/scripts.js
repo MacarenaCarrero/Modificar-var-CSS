4c4d0725ee75ac91b356059437704802ca4969c5
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-07 20:02:45 +0200

    var

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+// raton
 const mouseElement = document.getElementById('mouse');
+const redBoxElement = document.getElementById('redBox');
+const pixelsElement = document.getElementById('pixels');
+//
 
-mouseElement.addEventListener('mousemove');
+mouseElement.addEventListener('mousemove', mouseFollower);
